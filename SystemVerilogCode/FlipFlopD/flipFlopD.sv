4f1482431c758b9fc74d1e474cb3b3733a9a2c3a
Fabelifer2797 <fabelifer@gmail.com>
2021-10-27 13:29:01 -0600

    [ADD] Flip Flop D

---
+++ SystemVerilogCode/FlipFlopD/flipFlopD.sv
@@ -0,0 +1,11 @@
+module flipFlopD #(parameter N = 64)(input logic clk, reset,
+                                    input logic[N-1:0] d,
+                                    output logic [N-1:0] q);
+
+        always_ff @(posedge clk or posedge reset)
+
+            if (reset) q <= 0;
+            else q <= d;
+
+
+endmodule
