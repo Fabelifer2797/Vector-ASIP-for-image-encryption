63a48aa28d48b77683f2436168768249a6502990
Brad0914 <bradmvf09081999@gmail.com>
2021-11-03 15:01:42 -0600

    Script de image to txt terminado

--- ScriptsAltoNivel/img2txt.py
+++ ScriptsAltoNivel/img2txt.py
@@ -1,6 +1,17 @@
 from PIL import Image
 import numpy as np
 import os
+
+INT_BITS = 8
+## sacado de https://www.geeksforgeeks.org/rotate-bits-of-an-integer/
+def rightRotate(n, d):
+    # In n>>d, first d bits are 0.
+    # To put last 3 bits of at
+    # first, do bitwise or of n>>d
+    # with n <<(INT_BITS - d)
+    return (n >> d) | (n << (INT_BITS - d)) & 0xFFFFFFFF
+
+
 #### hace de img a txt
 im = Image.open('img.jpg')
 
@@ -14,6 +25,8 @@ pixeles = []
 pixelestxt = []
 pixarray = np.arange(0, x*y, 1, np.uint8)
 
+#### algoritmo original################
+
 for j in range(y):
     for i in range(x):
         pixeles.append(pix[i,j])
@@ -26,10 +39,76 @@ for i in range(x*y):
     
 file.close()
 
+##### archivo con el algoritmo xor
 
-#### Vuelve de txt a image
+pixelesXOR = []
+
+for i in range(x * y):
+    pixelesXOR.append(pixeles[i] ^ 32)
+
+file = open("imgEnTXTXOR.txt", "w")
+
+for i in range(x * y):
+    file.write(str(pixelesXOR[i]))
+    file.write('\n')
+
+file.close()
+
+##### archivo con el algoritmo shift
+
+pixelesShift = []
+
+for i in range(x * y):
+    pixelesShift.append(rightRotate(pixeles[i], 4))
+
+file = open("imgEnTXTshift.txt", "w")
+
+for i in range(x * y):
+    file.write(str(pixelesShift[i]))
+    file.write('\n')
+
+file.close()
+
+#### Vuelve de txt a image#############
+
+# lines = []
+# with open('imgEnTXT.txt') as f:
+#     lines = f.readlines()
+#
+# counter = 0
+#
+# for line in lines:
+#     pixarray[counter] = int(line.strip())
+#     counter += 1
+#
+#
+# pixarray = np.reshape(pixarray, (y, x))
+#
+# new_image = Image.fromarray(pixarray)
+# new_image.save('new.jpg')
+
+#############################################
+
+# lines = []
+# with open('imgEnTXTXOR.txt') as f:
+#     lines = f.readlines()
+#
+# counter = 0
+#
+# for line in lines:
+#     pixarray[counter] = int(line.strip())
+#     counter += 1
+#
+#
+# pixarray = np.reshape(pixarray, (y, x))
+#
+# new_image = Image.fromarray(pixarray)
+# new_image.save('xor.jpg')
+
+################################################
+
 lines = []
-with open('imgEnTXT.txt') as f:
+with open('imgEnTXTshift.txt') as f:
     lines = f.readlines()
 
 counter = 0
@@ -37,9 +116,13 @@ counter = 0
 for line in lines:
     pixarray[counter] = int(line.strip())
     counter += 1
-    
-    
+
+
 pixarray = np.reshape(pixarray, (y, x))
 
 new_image = Image.fromarray(pixarray)
-new_image.save('new.jpg')
+new_image.save('shift.jpg')
+
+
+
+#   https://www.geeksforgeeks.org/python-bitwise-operators/
